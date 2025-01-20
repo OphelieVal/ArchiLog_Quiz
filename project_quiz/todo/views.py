6eadc225d8f6ce6bdf6512a589ab25bd93a30b98
OphelieVal <ophelie.valin@etu.uni-orleans.fr>
2025-01-20 09:18:36 +0100

    Résolution erreur 500 pour get_task()

--- project_quiz/todo/views.py
+++ project_quiz/todo/views.py
@@ -1,6 +1,7 @@
 from flask import jsonify, abort, make_response, request, url_for
 from .app import app
 from .models import tasks
+
 def make_public_task(task):
     new_task = {}
     for field in task :
@@ -11,14 +12,16 @@ def make_public_task(task):
             new_task[field] = task[field]
     return new_task
     
-@app.route('/todo/api/v1.0/tasks', methods = [' GET '])
+@app.route('/todo/api/v1.0/tasks', methods = ['GET'])
 def get_tasks():
-    return jsonify(tasks =[make_public_task(t) for t in tasks])
+    return jsonify(tasks =[make_public_task(t) for t in tasks])  #return un objet de type Response
 
 @app.route('/todo/api/v1.0/tasks/<int:task_id>', methods = ['GET'])
 def get_task(task_id):
-    tasks = get_tasks
-    return task_id in tasks
+    for t in tasks:
+        if t["id"] == task_id:
+            return make_public_task(t)
+    return 'Task not found'
 
 
 @app.route('/todo/api/v1 .0/tasks', methods = ['POST'])
@@ -26,9 +29,9 @@ def create_task() :
     if not request.json or not 'title' in request.json :
         abort(400)
         task = {
-            'id': tasks [ -1]['id'] + 1 ,
-            'title': request . json ['title'],
-            'description': request . json . get ('description', " " ) ,
+            'id': tasks[-1]['id'] + 1 ,
+            'title': request.json['title'],
+            'description': request.json.get('description', " " ),
             'done': False
         }
     tasks.append(task)
