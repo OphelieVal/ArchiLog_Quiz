f3895218b239e1ccd6f3d187ce3a6cb6dd5d457e
OphelieVal <ophelie.valin@etu.uni-orleans.fr>
2025-01-20 09:22:50 +0100

    Ajout route erreurs not found + bad request

--- project_quiz/todo/views.py
+++ project_quiz/todo/views.py
@@ -36,3 +36,12 @@ def create_task() :
         }
     tasks.append(task)
     return jsonify({'task': make_public_task(task)}), 201
+
+
+@app.errorhandler(404)
+def not_found(error) :
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
+@app.errorhandler(400)
+def not_found(error) :
+    return make_response(jsonify({'error': 'Bad request'} ) , 400)
